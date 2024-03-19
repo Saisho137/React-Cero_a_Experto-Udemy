2b80766c3d099cc2320b9d4eb854b883f4416056
Santiago <santib.2001@hotmail.com>
2024-03-18 23:28:43 -0500

    feat: Implemented click handler

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,10 +1,15 @@
 import PropTypes from 'prop-types';
 
 const CounterApp = ({value}) => {
+    const handleClick = (event) => {
+        console.log(event)
+    }
+
     return (
         <>
             <h1>CounterApp</h1>
-            <h2>{ value }</h2>
+            <h2> { value } </h2>
+            <button onClick={ handleClick }> +1 </button>
         </>
     )
 }
