add5d7dbf13287e56927c062a95d6f524bd0a989
Santiago <santib.2001@hotmail.com>
2024-03-20 20:17:54 -0500

    feat: Added list Render functionality

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react"
+
 const GifExpertApp = () => {
+    const [categories, setCategories] = useState(['Hola', 'Mundo']);
+
+    const onAddCategory = () => {
+        setCategories([...categories, 'Nueva categoria']);
+    }
+
     return (
         <>
             <h1>Gif Expert App</h1>
+            <button onClick={onAddCategory}>Agregar Categoria</button>
+            <ol>
+                {categories.map(category => (
+                    <li key={category}>{category}</li>
+                ))}
+            </ol>
         </>
     )
 }
--- 04-gif-expert-app/src/main.jsx
+++ 04-gif-expert-app/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './styles.css';
 import GifExpertApp from './GifExpertApp.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
