0fffe075cd78dfccc21f6653cb7265a80ac749db
Santiago <santib.2001@hotmail.com>
2024-04-10 21:08:28 -0500

    feat: Added Test Mocking a Custom Hook

--- 04-gif-expert-app/tests/components/GifGrid.test.jsx
+++ 04-gif-expert-app/tests/components/GifGrid.test.jsx
@@ -1,11 +1,36 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import GifGrid from "../../src/components/GifGrid";
+import useFetchGifs from "../../src/hooks/useFetchGifs";
+
+jest.mock('../../src/hooks/useFetchGifs');
 
 describe('Pruebas en <GifGrid/>', () => {
     const category = 'Evangelion';
-    test('Debe de mostrar el loading inicialmente!', () => {
+
+    test('Debe de mostrar el loading inicialmente', () => {
+        useFetchGifs.mockReturnValue({
+            images: [],
+            isLoading: true
+        });
+
         render(<GifGrid category={category} />);
+
         expect(screen.getByText('Cargando...'));
         expect(screen.getByText(category));
+    });
+
+    test('Debe de mostrar items cuando se cargan las imagenes', () => {
+        const gifs = [
+            {id: 'ABC', title: 'Shinji', url: 'https://localhost/shinji.jpg'},
+            {id: '123', title: 'Yuji', url: 'https://localhost/yuji.jpg'},
+        ]
+        useFetchGifs.mockReturnValue({
+            images: gifs,
+            isLoading: true
+        });
+        
+        render(<GifGrid category={category} />);
+
+        expect(screen.getAllByRole('img').length).toBe(2);
     });
 });
